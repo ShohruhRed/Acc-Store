182c607cefc5b6ab8ee8a81c0a4bda184f331bae
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-07-16 19:07:32 +0500

    added DbContex, Models, Migrations

---
+++ Acc.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Acc.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acc.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Acc.Services.ProductAPI/Program.cs
+++ Acc.Services.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using Acc.Services.ProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace Acc.Services.ProductAPI
 {
     public class Program
@@ -12,6 +15,7 @@ namespace Acc.Services.ProductAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
