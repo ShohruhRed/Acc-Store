852caac82b1cc45bf465b942cee969456e25a467
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-07-17 21:00:39 +0500

    Base interface for services and response model

---
+++ Acc.Web/Services/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using Acc.Web.Models;
+using Acc.Web.Services.IServices;
+
+namespace Acc.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseDto { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Acc.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Acc.Web.Models;
+
+namespace Acc.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseDto { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest)
+    }
+}
