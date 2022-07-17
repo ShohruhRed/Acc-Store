e6478836ec38ac06188fc5b4c92ed9e4e607f9a7
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-07-17 16:30:26 +0500

    implementation of communication with the main project + ServiceURL link

---
+++ Acc.Web/Models/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Acc.Web.Models
+{
+    public class ResponseDto
+    {
+        public bool IsSucces { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}
