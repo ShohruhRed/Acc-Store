c1cfcf618a60bcc754619bb0525d3acd0402f70c
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-07-18 19:28:37 +0500

    Configuring access to the product management service and Http client

--- Acc.Web/Program.cs
+++ Acc.Web/Program.cs
@@ -1,3 +1,7 @@
+using Acc.Web.Services;
+using Acc.Web.Services.IServices;
+
+
 namespace Acc.Web
 {
     public class Program
@@ -7,6 +11,10 @@ namespace Acc.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddHttpClient<IProductService, ProductService>();
+            SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
+            builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
