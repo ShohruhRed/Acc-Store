29741dfb0bef4cf92b18aa5fb301f8863d3f2d93
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-07-19 13:29:28 +0500

    A controller for managing products and a method for getting a list of Products

---
+++ Acc.Web/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Acc.Web.Models;
+using Acc.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Acc.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new();
+
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+
+            if(response != null && response.IsSucces)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
