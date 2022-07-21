958c3fcf4d46904025be577398865bee4fc157bb
Shohruh Abduvohidov <shohruhred@inbox.ru>
2022-07-21 19:04:02 +0500

    Configuring the use of Identification in the main Web project

--- Acc.Web/Program.cs
+++ Acc.Web/Program.cs
@@ -16,6 +16,25 @@ namespace Acc.Web
             SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddControllersWithViews();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = builder.Configuration["ServiceUrls:ProductAPI"];
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.ClientId = "acc";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+
+                    options.TokenValidationParameters.NameClaimType = "name";
+                    options.TokenValidationParameters.RoleClaimType = "role";
+                    options.Scope.Add("acc");
+                    options.SaveTokens = true;
+                });
 
             var app = builder.Build();
 
@@ -32,6 +51,7 @@ namespace Acc.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
